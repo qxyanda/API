75ab8794638b985db86f7cbc4100bf8e2d291723
qxyanda <qxyanda@163.com>
2021-01-29 12:01:09 +0800

    redis

--- test/EFCore.MySql.IntegrationTests/Controllers/PersonController.cs
+++ test/EFCore.MySql.IntegrationTests/Controllers/PersonController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using ViewModels;
 using System.Collections.Generic;
 using System;
+using StackExchange.Redis;
+using RedisDemo;
+using Newtonsoft.Json;
+
 
 namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Controllers
 {
@@ -11,31 +15,46 @@ namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Controllers
     public class PersonController : Controller
     {
 	    private readonly AppDb _db;
+        private readonly IDatabase _redis;
 
-	    public PersonController(AppDb db)
+        private readonly int cacheTime = 600;
+
+	    public PersonController(AppDb db,RedisHelper client)
 	    {
 		    _db = db;
+            _redis = client.GetDatabase();
 	    }
 
 	    // GET api/sync
         [HttpGet]
         public IActionResult Get()
         {
-            var models = _db.People.OrderBy(m => m.Id).Take(10).ToList();
+            Test();
+            // 从Redis里面取数据
+            string value = _redis.StringGet("/api/Person/");
+
+            if(!string.IsNullOrEmpty(value))
+            {
+                return new ObjectResult(JsonConvert.DeserializeObject(value));
+            }
+
+            // var models = _db.People.OrderBy(m => m.Id).Take(10).ToList();
 
             //等效
 
-            // var sqls =
-            // (
-            //     from m in _db.People
-            //     orderby m.Id
-            //     select m
-            // ).Take(10);
+            var sqls =
+            (
+                from m in _db.People
+                orderby m.Id
+                select new {m.Id,m.UserName}
+            ).Take(10);
 
-            // var models = sqls.ToList();
+            var models = sqls.ToList();
 
             if(models != null)
             {
+                // 往Redis里面存入数据
+                _redis.StringSet("/api/Person/", $"{JsonConvert.SerializeObject(models)}",TimeSpan.FromSeconds(cacheTime));
                 return new ObjectResult(models);
                 // List<Person> viewModels = new List<Person>();
                 // foreach( var model in models)
@@ -51,26 +70,35 @@ namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Controllers
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
+            // 从Redis里面取数据
+            string value = _redis.StringGet("/api/Person/"+id);
+
+            if(!string.IsNullOrEmpty(value))
+            {
+                return new ObjectResult(JsonConvert.DeserializeObject(value));
+            }
             //
             //SELECT
             //
-            var model = _db.People.Where(m => m.Id == id).Select(m => new {m.UserName,m.PassWord});
+            // var model = _db.People.Where(m => m.Id == id).Select(m => new {m.UserName, m.PassWord});
 
             // Console.WriteLine(_db.People.Where(m => m.Id == id).Select(m => new {m.UserName,m.PassWord}).ToString());
 
 			// var model = _db.People.FirstOrDefault(m => m.Id == id);
 
-            // var sqls =
-            // (
-            //     from m in _db.People
-            //     where m.Id == id
-            //     select new {m.UserName,m.PassWord}
-            // );
+            var sqls =
+            (
+                from m in _db.People
+                where m.Id == id
+                select new {m.Id,m.UserName}
+            );
 
-            // var model = sqls;
+            var model = sqls;
 
-			if (model != null)
+			if (model != null && !$"{JsonConvert.SerializeObject(model)}".Equals("[]"))
 			{
+                // 往Redis里面存入数据
+                _redis.StringSet("/api/Person/"+id, $"{JsonConvert.SerializeObject(model)}",TimeSpan.FromSeconds(cacheTime));
 				return new ObjectResult(model);
 			}
 			return NotFound();
@@ -81,6 +109,7 @@ namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Controllers
         {
 	        _db.People.Add(body);
 	        _db.SaveChanges();
+            _redis.KeyDelete("/api/Person/");
 			return new ObjectResult(body);
         }
 
@@ -93,6 +122,8 @@ namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Controllers
 			{
 				model.PassWord = body.PassWord;
 				_db.SaveChanges();
+                _redis.KeyDelete("/api/Person/");
+                _redis.KeyDelete("/api/Person/"+id);
 				return new ObjectResult(model);
 			}
 			return NotFound();
@@ -112,10 +143,20 @@ namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests.Controllers
                 }
                 _db.People.Remove(model);
                 _db.SaveChanges();
+                _redis.KeyDelete("/api/Person/");
+                _redis.KeyDelete("/api/Person/"+id);
                 return Ok();
             }
 
 			return NotFound();
+        }
+
+        public void Test()
+        {
+            // string path = @"d:\test\123.sqlite";
+            // SqliteConnection cn = new SQLiteConnection("data source=" + path);
+            // cn.Open();
+            // cn.Close();
         }
     }
 }
--- test/EFCore.MySql.IntegrationTests/Startup.cs
+++ test/EFCore.MySql.IntegrationTests/Startup.cs
@@ -13,6 +13,7 @@ using MySqlConnector;
 using Newtonsoft.Json;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Pomelo.EntityFrameworkCore.MySql.Tests;
+using RedisDemo;
 
 namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests
 {
@@ -73,6 +74,17 @@ namespace Pomelo.EntityFrameworkCore.MySql.IntegrationTests
                 .AddDefaultTokenProviders();
 
             services.AddControllers();
+
+            //redis缓存
+            var section = Configuration.GetSection("Redis:Default");
+            //连接字符串
+            string _connectionString = section.GetSection("Connection").Value;
+            //实例名称
+            string _instanceName = section.GetSection("InstanceName").Value;
+            //默认数据库
+            int _defaultDB = int.Parse(section.GetSection("DefaultDB").Value ?? "0");
+            services.AddSingleton(new RedisHelper(_connectionString, _instanceName, _defaultDB));
+
         }
 
         public static void ConfigureEntityFramework(IServiceCollection services)
